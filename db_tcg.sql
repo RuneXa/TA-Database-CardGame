4374566b4767556618ee21ae4306914f1c021c34
VCoklat <dVanHauten@gmail.com>
2015-05-28 22:23:08 +0700

    update nama column

--- db_tcg.sql
+++ db_tcg.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 28, 2015 at 03:54 PM
+-- Generation Time: May 28, 2015 at 05:21 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `tb_datamusuh` (
 CREATE TABLE IF NOT EXISTS `tb_deck` (
   `id` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `kodekartu` int(11) NOT NULL
+  `kode_kartu` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -53,7 +53,6 @@ CREATE TABLE IF NOT EXISTS `tb_deck` (
 --
 
 CREATE TABLE IF NOT EXISTS `tb_kartu` (
-`id` int(11) NOT NULL,
   `kode_kartu` int(11) NOT NULL,
   `efek` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -79,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `tb_user` (
 
 CREATE TABLE IF NOT EXISTS `tb_winrate` (
 `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `win` int(11) NOT NULL,
   `loss` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -98,13 +97,13 @@ ALTER TABLE `tb_datamusuh`
 -- Indexes for table `tb_deck`
 --
 ALTER TABLE `tb_deck`
- ADD UNIQUE KEY `id_user_2` (`id_user`), ADD KEY `kodekartu` (`kodekartu`), ADD KEY `id_user` (`id_user`);
+ ADD UNIQUE KEY `id_user_2` (`id_user`), ADD KEY `kodekartu` (`kode_kartu`), ADD KEY `id_user` (`id_user`);
 
 --
 -- Indexes for table `tb_kartu`
 --
 ALTER TABLE `tb_kartu`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `kode_kartu` (`kode_kartu`), ADD UNIQUE KEY `kode_kartu_2` (`kode_kartu`), ADD KEY `kode_kartu_3` (`kode_kartu`);
+ ADD PRIMARY KEY (`kode_kartu`), ADD UNIQUE KEY `kode_kartu` (`kode_kartu`), ADD UNIQUE KEY `kode_kartu_2` (`kode_kartu`), ADD KEY `kode_kartu_3` (`kode_kartu`);
 
 --
 -- Indexes for table `tb_user`
@@ -116,7 +115,7 @@ ALTER TABLE `tb_user`
 -- Indexes for table `tb_winrate`
 --
 ALTER TABLE `tb_winrate`
- ADD PRIMARY KEY (`id`), ADD KEY `user_id` (`user_id`), ADD KEY `user_id_2` (`user_id`);
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id_user` (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -126,11 +125,6 @@ ALTER TABLE `tb_winrate`
 -- AUTO_INCREMENT for table `tb_datamusuh`
 --
 ALTER TABLE `tb_datamusuh`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `tb_kartu`
---
-ALTER TABLE `tb_kartu`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `tb_winrate`
@@ -145,14 +139,14 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 -- Constraints for table `tb_deck`
 --
 ALTER TABLE `tb_deck`
-ADD CONSTRAINT `tb_deck_ibfk_1` FOREIGN KEY (`kodekartu`) REFERENCES `tb_kartu` (`kode_kartu`),
-ADD CONSTRAINT `tb_deck_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`);
+ADD CONSTRAINT `tb_deck_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`),
+ADD CONSTRAINT `tb_deck_ibfk_3` FOREIGN KEY (`kode_kartu`) REFERENCES `tb_kartu` (`kode_kartu`);
 
 --
 -- Constraints for table `tb_winrate`
 --
 ALTER TABLE `tb_winrate`
-ADD CONSTRAINT `tb_winrate_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tb_user` (`id_user`);
+ADD CONSTRAINT `tb_winrate_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
