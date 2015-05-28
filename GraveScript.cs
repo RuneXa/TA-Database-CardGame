36987370d75e3b12c81a24820aec65a6cebf1f37
RuneXa <richiebachtiar@gmail.com>
2015-05-28 11:48:29 +0700

    Script Untuk Graveyard zone

---
+++ GraveScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraveScript : MonoBehaviour {
+
+	public void Update(){
+		if(this.transform.childCount > 0){
+			for(int i = 0; i < this.transform.childCount; i++){
+				this.transform.GetChild(i).position = this.transform.position;
+			}
+		}
+	}
+
+}
