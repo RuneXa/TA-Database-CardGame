091f24b5db472bd02d6259a83236ca2a12f62b2a
RuneXa <richiebachtiar@gmail.com>
2015-05-28 19:20:53 +0700

    Project Folder
    Nanti buka dari sini aja deh, jadi folder unity-nya sama semua, sama
    semua projectnya..
    
    btw, Script segala macem ada di folder Asset

---
+++ ProjectFolder/Assets/DeckScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeckScript : MonoBehaviour {
+
+	public GameObject card;
+	GameObject cardCpy;
+
+
+	public void Draw () {
+		cardCpy = Transform.Instantiate(card,new Vector3(Input.mousePosition.x,Input.mousePosition.y,Input.mousePosition.z),card.transform.rotation) as GameObject; 
+		cardCpy.transform.SetParent(this.transform.parent.parent.FindChild("Hand"));
+	}
+
+
+}
