1996e2d813e369dea8d4e1ad59f283586d2fcce5
Charon77 <evans.jahja@yahoo.com>
2015-05-29 09:08:48 +0700

    PHP mulai bisa :D

--- ProjectFolder/Assets/DeckScript.cs
+++ ProjectFolder/Assets/DeckScript.cs
@@ -5,12 +5,21 @@ public class DeckScript : MonoBehaviour {
 
 	public GameObject card;
 	GameObject cardCpy;
-
-
+	
 	public void Draw () {
+		StartCoroutine (fetchUrl());
 		cardCpy = Transform.Instantiate(card,new Vector3(Input.mousePosition.x,Input.mousePosition.y,Input.mousePosition.z),card.transform.rotation) as GameObject; 
 		cardCpy.transform.SetParent(this.transform.parent.parent.FindChild("Hand"));
 	}
+	
 
+	IEnumerator fetchUrl()
+	{
+		WWW www = new WWW("http://localhost/card.php");
+		yield return www.isDone;
+		for (int i=0; i<www.bytesDownloaded; i++) {
+			Debug.Log((char)www.bytes[i]);
+		}
 
+	}
 }
