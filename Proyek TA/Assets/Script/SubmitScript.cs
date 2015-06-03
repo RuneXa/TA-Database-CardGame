10d8c40c8cd26cefec1c085a30a9cb597f5a1884
Charon77 <evans.jahja@yahoo.com>
2015-06-03 12:01:50 +0700

    Deck Editor Submissive :')

---
+++ Proyek TA/Assets/Script/SubmitScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubmitScript : MonoBehaviour {
+
+	public void onClick()
+	{
+		Transform deckZone = this.transform.parent.parent.FindChild ("DeckZone");
+		foreach (Transform kartu in deckZone)
+		{
+			Debug.Log(kartu.GetComponent<CardScript>().kode);
+		}
+	}
+}
